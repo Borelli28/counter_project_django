b323a9cc01661012cbbe5db290938a4cbc9a212d
armando <borelliarmando12@gmail.com>
2021-02-13 22:28:15 -0600

    counter increments by 1 everytime index.html is visited, but cannot make the destroy(reset) page works

--- counter_proj_app/views.py
+++ counter_proj_app/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')")
+    print("Inside of Index")
+    request.session['counter'] += 1
+    print(request.session['counter'])
+    return render(request, 'index.html')
+
+# def destroy(request):
+#     print('redirected boyyy!')
+#     del request.session['counter']
+#     print("Destroy: And now counter was reset")
+#     return redirect('/')
