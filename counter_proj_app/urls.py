51f883f67ea567cd80f8f5e1b2aab608f6682212
armando <borelliarmando12@gmail.com>
2021-02-13 23:14:57 -0600

    I read the documentation and I think I figure the sessions thing views, now still working to make it work in destroy mehtod

--- counter_proj_app/urls.py
+++ counter_proj_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    # path('destroy', views.destroy)	   
+    path('destroy', views.destroy)	   
 ]
--- counter_proj_app/views.py
+++ counter_proj_app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import redirect, render, HttpResponse
 
 def index(request):
-    print("Inside of Index")
-    request.session['counter'] += 1
-    print(request.session['counter'])
+    # counter key exist in session, increment counter by 1
+    if 'counter' in request.session:
+        request.session['counter'] += 1
+        print(request.session['counter'])
+    # If counter does not exist create counter and assign value of 1
+    else:
+        request.session['counter'] = 1
+
     return render(request, 'index.html')
 
-# def destroy(request):
-#     print('redirected boyyy!')
-#     del request.session['counter']
-#     print("Destroy: And now counter was reset")
-#     return redirect('/')
+def destroy(request):
+    print('redirected boyyy!')
+    # request.session.clear()
+    print("Destroy: And now counter was reset")
+    return redirect('/')
