2d4efd1cf8590c51e5f20ccd420d090f4c750487
armando <borelliarmando12@gmail.com>
2021-02-13 23:22:42 -0600

    added comments

--- counter_proj_app/views.py
+++ counter_proj_app/views.py
@@ -5,7 +5,7 @@ def index(request):
     if 'counter' in request.session:
         request.session['counter'] += 1
         print(request.session['counter'])
-    # If counter does not exist create counter and assign value of 1
+    # If counter does not exist create counter and assign value of 1 to counter
     else:
         request.session['counter'] = 1
 
@@ -13,10 +13,12 @@ def index(request):
 
 def destroy(request):
     print('redirected boyyy!')
+    # counter exist in session; then clear that mf
     if 'counter' in request.session:
         request.session.clear()
         print("Destroy: And now counter was reset")
+    # else it will print to console that session is empty
     else:
         print(" 'counter' does not exist in session so, it was not reseted ")
-
+    # Redirect to root
     return redirect('/')
